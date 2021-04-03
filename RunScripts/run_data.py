692eb39e8dcb4ccbbc5ac96e3bc5450df9058092
Shalev <shalev.druk@walla.com>
2021-04-03 09:41:57 +0300

    Fix output script merger

--- RunScripts/run_data.py
+++ RunScripts/run_data.py
@@ -26,19 +26,19 @@ def get_locations(content, i, res):
     while i < len(content):
         i += 1
         if "Enter Location" in content[i] or "Exit Location" in content[i]:
-            res.append(re.findall(r"[-+]?\d*\.\d+|\d+", content[i]))
+            res.append(str(re.findall(r"[-+]?\d*\.\d+|\d+", content[i])))
         elif "Particle Volume" in content[i] or "Number of" in content[i]:
             break
-    if len(res) < old_len+2: # in case the particle stopped in this slab
-        res.append(['-','-','-'])
+    if len(res) < old_len + 2:  # in case the particle stopped in this slab
+        res.append('None')
     return i
 
 
 def add_missing_slabs(res):
     global NUMBER_OF_SLABS
     while NUMBER_OF_SLABS < 0:
-        res.append(['-','-','-'])
-        res.append(['-','-','-'])
+        res.append('None')
+        res.append('None')
         NUMBER_OF_SLABS -= 1
 
 
@@ -57,9 +57,10 @@ def get_run_data(run_dir):
     res = list()
     with open("{}/{}".format(run_dir, OUTPUT_FILE_NAME), 'r') as output_file:
         file_lines = output_file.readlines()
-    content = [line.rstrip('\n') for line in file_lines]
-    i = 0
+        content = [line.rstrip('\n') for line in file_lines]
+    i = 1
     while i < len(content):
+        print('current line: {}'.format(content[i]))
         # Getting locations
         if "Particle Volume" in content[i]:
             i = get_locations(content, i, res)
@@ -73,7 +74,12 @@ def get_run_data(run_dir):
         elif "Scintillator" in content[i]:
             res.append(get_particle_count(content[i], 2))
         i += 1
+    return res
 
+
+def get_run_number(run_dir):
+    # BEN LAMI ADD YOUR CODE HERE #
+    pass
 
 if __name__ == "__main__":
     save_path = sys.argv[1]
@@ -83,14 +89,15 @@ if __name__ == "__main__":
     else:
         exit(1)
 
-    simulation_data = list()
+    simulation_data = dict()
     for run_dir, _, filename in os.walk(os.getcwd()):
         if INPUT_FILE_NAME in filename:
             os.chdir(run_dir)
             # with open("run_stdout.txt", 'w') as out_fd:
             #     subprocess.run([GEANT_EXE_LOCATION, INPUT_FILE_NAME], stdout=out_fd)
-            simulation_data.append(get_run_data(run_dir))
-            print('Debug: extracted the following data: \n {}'.format(simulation_data[len(simulation_data)-1]))
+            run_number = get_run_number(run_dir)
+            simulation_data[run_number] = get_run_data(run_dir)
+            print('Debug: extracted the following data: \n {}'.format(simulation_data[run_number]))
     
     with open("{}/mapping.csv".format(save_path),'r') as csvinput:
         with open("{}/final_results.csv".format(save_path), 'w') as csvoutput:
@@ -99,11 +106,14 @@ if __name__ == "__main__":
 
             output_lines = []
             row = next(reader)
-            row.extend(['Silicon_2 enter location', 'Silicon_2 exit location', 'Silicon_1 enter location', 'Silicon_1 exit location', 'Scint_1 enter location', 'Scint_1 exit location', 'Scint_2 enter location', 'Scint_2 exit location', 'Number of photons created', 'Number of electron-hole pairs created', 'Photons absorbed in Scint_1 Top-Left', 'Photons absorbed in Scint_1 Bottom-Right', 'Photons absorbed in Scint_1 Top-Right', 'Photons absorbed in Scint_1 Bottom-Left', 'Photons absorbed in Scint_2 Top-Left', 'Photons absorbed in Scint_2 Bottom-Right', 'Photons absorbed in Scint_2 Top-Right', 'Photons absorbed in Scint_2 Bottom-Left'])
+            row.extend(['Silicon_2 enter location', 'Silicon_2 exit location', 'Silicon_1 enter location', 'Silicon_1 exit location', 'Scint_1 enter location', 'Scint_1 exit location', 'Scint_2 enter location', 'Scint_2 exit location', 'Number of photons created', 'Number of electron-hole pairs created in Silicon_1', 'Number of electron-hole pairs created in Silicon_2', 'Photons absorbed in Scint_1 Top-Left', 'Photons absorbed in Scint_1 Bottom-Right', 'Photons absorbed in Scint_1 Top-Right', 'Photons absorbed in Scint_1 Bottom-Left', 'Photons absorbed in Scint_2 Top-Left', 'Photons absorbed in Scint_2 Bottom-Right', 'Photons absorbed in Scint_2 Top-Right', 'Photons absorbed in Scint_2 Bottom-Left'])
             output_lines.append(row)
             print(simulation_data)
-            for i,row in enumerate(reader):
-                row.extend(simulation_data[i])
+            for run_number, row in enumerate(reader):
+                if run_number in simulation_data:
+                    row.extend(simulation_data[run_number])
+                else:
+                    print('Can\'t find output data for run number: {}'.format(run_number))
                 output_lines.append(row)
 
             writer.writerows(output_lines)
